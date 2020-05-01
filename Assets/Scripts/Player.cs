3bff5a2def596ab1c63b4f0a6fbdcd1e84e609f6
Stangil <stangil44@gmail.com>
2020-05-01 16:16:43 -0400

    Added Player ship and movment

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 15.0f;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+        var newXPos = transform.position.x + deltaX;
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
