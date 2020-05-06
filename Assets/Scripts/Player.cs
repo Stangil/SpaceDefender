aa6d6fca23e20047b19e700f7d5c6bf7714704c4
Stangil <stangil44@gmail.com>
2020-05-06 15:28:56 -0400

    Limit ships movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 15.0f;
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    [SerializeField] float playerLimitMax = 0.95f;
+    [SerializeField] float playerLimitMin = 0.05f;
     void Start()
     {
-        
+        SetUpMoveBoundaries();
     }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(playerLimitMin, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(playerLimitMax, 0, 0)).x;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, playerLimitMin, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, playerLimitMax, 0)).y;
+    }
+
     void Update()
     {
         Move();
@@ -17,8 +34,9 @@ public class Player : MonoBehaviour
     {
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newYPos = transform.position.y + deltaY;
-        var newXPos = transform.position.x + deltaX;
+
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
         transform.position = new Vector2(newXPos, newYPos);
     }
 }
